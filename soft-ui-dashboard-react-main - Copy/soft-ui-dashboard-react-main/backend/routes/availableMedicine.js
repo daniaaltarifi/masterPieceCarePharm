494922c73777a66a3ed8886e8fc9f29bce133e7a
daniaaltarifi <127290875+daniaaltarifi@users.noreply.github.com>
2023-09-05 14:18:50 +0300

    Delete soft-ui-dashboard-react-main - Copy/soft-ui-dashboard-react-main directory

--- soft-ui-dashboard-react-main - Copy/soft-ui-dashboard-react-main/backend/routes/availableMedicine.js
+++
@@ -1,9 +0,0 @@
-const router=require('express').Router()
-const medicineAvailable=require('../models/medicineAvailableModel.js')
-const {addMedicineAvailable,getAllAvailable,updateMedicineAvailable,getAvailableById,delAvailableInfo}=require('../controller/medicineAvailbleController.js')
-router.route('/').get(getAllAvailable)
-router.route('/getAvilableById/:_id').get(getAvailableById)
-router.route('/add').post(addMedicineAvailable)
-router.route('/update/:_id').patch(updateMedicineAvailable)
-router.route('/del/:_id').delete(delAvailableInfo)
-module.exports=router
--- soft-ui-dashboard-react-main - Copy/soft-ui-dashboard-react-main/backend/routes/pharmacy.js
+++
@@ -1,8 +0,0 @@
-const router=require("express").Router();
-const {getAllPharmacy,getPharmacyById,postPharmacy,updatePharmacy,deletePharmacy}=require('../controller/pharmacyController.js');
-router.route('/').get(getAllPharmacy)
-router.route('/get/:_id').get(getPharmacyById)
-router.route('/post').post(postPharmacy)
-router.route('/update/:_id').patch(updatePharmacy)
-router.route('/delete/:_id').delete(deletePharmacy)
-module.exports=router
